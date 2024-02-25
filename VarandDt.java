eb3dfe9e59ffc914230e1c781735545019bb3f7f
Pranjal0901 <137981201+Pranjal0901@users.noreply.github.com>
2024-02-25 16:44:16 +0530

    first commit

---
+++ VarandDt.java
@@ -0,0 +1,10 @@
+public class VarandDt {
+    public static void main(String[] args) {
+// Sum of 2 Numbers-------
+        int a=10;
+        int b=5;
+        int sum=a+b;
+        System.out.println(sum);
+    // Output-15
+    }
+}
