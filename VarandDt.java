0e867436a3ce1e13ab3e5461e94bcc656715e5a0
Pranjal0901 <137981201+Pranjal0901@users.noreply.github.com>
2024-02-25 17:16:03 +0530

    First commit

--- VarandDt.java
+++ VarandDt.java
@@ -7,13 +7,19 @@ public class VarandDt {
         int sum=a+b;
         System.out.println(sum);
     // Output-15
-// ------------------------------------------------------------------------
-// Taking input in Java
+
+    // Taking input in Java
         Scanner s=new Scanner(System.in);
         int num = s.nextInt();
         System.out.println(num);
+        /*Input-98  || Output-98 */
+
+// Taking input from user and calculate the sum of 2 numbers
+        int num1=s.nextInt();
+        int num2=s.nextInt();
+        int add=num1+num2;
+        System.out.println("The sum of 2 numbers is " + add);
         s.close();
-        /*Input-98
-          Output-98 */
+        /* Input=65,56 || Output=121 */ 
     }
 }
